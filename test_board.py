b9ec0fe75a5395d41ed8e7235b5a2a9407b0c3d1
Jakub Kryczka <kryczka.jakub.pg2@gmail.com>
2023-01-07 15:28:32 +0100

    Finished testing the Board class

--- test_board.py
+++ test_board.py
@@ -1,12 +1,13 @@
 from board import (
     Board,
     BoardError,
-    WrongWordError,
-    NotConnectedError
+    WrongWordError
     )
 from settings import boardSize as BSIZE
 from pytest import raises
 from random import choice
+from field import FieldError
+from checkDict import createDict
 
 
 def test_boardInit():
@@ -48,9 +49,65 @@ def test_insertVertical():
     assert word in board.getBoardWords()[0]
 
 
-def test_insertOccupied():
+def test_insertOutOfBounds():
     board = Board()
     reference = Board()
     word = "SLOWO"
     with raises(WrongWordError):
         board.insertVertical(word, ("A", BSIZE + 1), reference)
+
+
+def test_insertOccupied():
+    board = Board()
+    reference = Board()
+    word = "SLOWO"
+    board.insertVertical(word, ("A", 1), reference)
+    secondWord = "DRUGIE"
+    with raises(FieldError):
+        board.insertHorizontal(secondWord, ("A", 1), reference)
+
+
+def test_insertConnected():
+    board = Board()
+    reference = Board()
+    word = "SLOWO"
+    board.insertVertical(word, ("A", 1), reference)
+    secondWord = "PAĆ"
+    board.insertHorizontal(secondWord, ("A", 2), reference)
+    assert "SPAĆ" in board.getBoardWords()
+
+
+def test_emptyBoardValidation():
+    board = Board()
+    assert board.validateBoard() is True
+
+
+def test_oneWordBoardValidation():
+    board = Board()
+    reference = Board()
+    createDict()
+    word = "SŁOWO"
+    board.insertVertical(word, ("A", 1), reference)
+    assert board.validateBoard() is True
+
+
+def test_multiWordBoardValidation():
+    board = Board()
+    reference = Board()
+    createDict()
+    word = "SŁOWO"
+    board.insertVertical(word, ("A", 1), reference)
+    secondWord = "PAĆ"
+    board.insertHorizontal(secondWord, ("A", 2), reference)
+    thirdWord = "MA"
+    board.insertVertical(thirdWord, ("B", 4), reference)
+    assert board.validateBoard() is True
+
+
+def test_failedBoardValidation():
+    board = Board()
+    reference = Board()
+    createDict()
+    word = "GHYUHD"
+    board.insertVertical(word, ("A", 1), reference)
+    assert board.validateBoard() is False
